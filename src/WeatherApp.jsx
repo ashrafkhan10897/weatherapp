925670155f5bb7c80ea9977355e28f243e72eb4a
Ashrafkhan <ashrafkhan10897@gmail.com>
2024-02-28 11:54:38 +0530

    Update WeatherApp.jsx

--- src/WeatherApp.jsx
+++ src/WeatherApp.jsx
@@ -5,7 +5,7 @@ import { useState } from "react"
 export default function WeatherApp(){
     const [weatherInfo,setWeatherInfo] = useState({
         city:"Adilabad",
-        feelslike:24.84,
+        feelsLike:24.84,
         temp: 25.05,
         tempMin: 25.05,
         tempMax: 25.05,
