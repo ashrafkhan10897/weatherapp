7d9097cdce1142c9d6a2e6927cdcab329b133f66
Ashrafkhan <ashrafkhan10897@gmail.com>
2024-02-28 11:49:51 +0530

    Update InfoBox.jsx

--- src/InfoBox.jsx
+++ src/InfoBox.jsx
@@ -30,7 +30,7 @@ export default function InfoBox({info}){
                         <p>Humidity = {info.humidity}</p>
                         <p>Min Temp = {info.tempMin}&deg;C</p>
                         <p>Max Temp = {info.tempMax}&deg;C</p>
-                        <p>The weather can be described as <i>{info.weather}</i> and feels like = {info.feelslike}&deg;C</p>
+                        <p>The weather can be described as <i>{info.weather}</i> and feels like = {info.feelsLike}&deg;C</p>
                     </Typography>
                     </CardContent>
                 </Card>
